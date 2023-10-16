13f578c554673be13f0467105ddb0bfff7f9209b
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2023-10-16 23:59:02 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,35 @@
-var mSend = document.getElementById("sender");
-var mReciv = "6281311924726";
+var mReciv = document.getElementById("recive");
 var mSub = document.getElementById("subject");
-
-var instanceId = "YOUR_API_KEY";
+var instanceId = ""; // YOUR API_KEY
+var mSends = ""; // YOUR NUMBER
+var mImg = document.getElementById("gambar");
 
 function btnSendWApi() {
-  if (mSend < 0 || mReciv < 0 || mSub < 0) {
+  if (mReciv.value < 1 || mSub.value < 1) {
     alert("Harap isi semua bidang");
   } else {
-    let uri_wapi = "https://wa.srv16.wapanels.com/send-message?api_key="+instanceId+"&sender="+mReciv+"&number="+mSend.value+"&message="+mSub.value;
-    fetch(uri_wapi, {
-      method: 'GET',
-      headers: {
-          'Accept': 'application/json',
-      },
-    })
-    .then(response => response.json())
-    .then(response => console.log(JSON.stringify(response)))
+    if (mImg.value < 1) {
+      var uri_wapi = "https://wa.srv16.wapanels.com/send-message?api_key="+instanceId+"&sender="+mSends+"&number="+mReciv.value+"&message="+mSub.value+" *Example by WAPI*";
+      fetch(uri_wapi, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+        },
+      })
+      .then(response => response.json())
+      .then(response => console.log(JSON.stringify(response)))
+      console.log(uri_wapi);
+    } else {
+      var uri_wapi = "https://wa.srv16.wapanels.com/send-media?api_key="+instanceId+"&sender="+mSends+"&number="+mReciv.value+"&media_type=image&caption="+mSub.value+" *Example by WAPI*"+"&url="+mImg.value;
+      fetch(uri_wapi, {
+        method: 'GET',
+        headers: {
+            'Accept': 'application/json',
+        },
+      })
+      .then(response => response.json())
+      .then(response => console.log(JSON.stringify(response)))
+      console.log(uri_wapi);
+    }
   }
 }
